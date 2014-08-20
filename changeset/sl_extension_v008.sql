6f51477dd310a131fd326fda1813f0fd29d5a79a
amcdowell <armcdowell@hotmail.com>
2014-08-20 14:52:18 +1200

    Caching Dynamic Queries for Map Navigation [#4 responsible:none state:fixed]
    Updated SearchEJB to cache the Dynamic Queries used for map
    navigation as this improves the responsiveness of the map.

---
+++ changeset/sl_extension_v008.sql
@@ -0,0 +1,13 @@
+﻿-- 20 Aug 2014
+ -- Fix the document nr number sequency
+DROP SEQUENCE document.document_nr_seq;
+
+CREATE SEQUENCE document.document_nr_seq
+  INCREMENT 1
+  MINVALUE 1000
+  MAXVALUE 99999999
+  START 1000
+  CACHE 1
+  CYCLE;
+COMMENT ON SEQUENCE document.document_nr_seq
+  IS 'Sequence number used as the basis for the document Nr field. This sequence is used by the Digital Archive EJB.';
