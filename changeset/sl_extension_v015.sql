d960670c30613a6b575a480bcd6a83719a963292
amcdowell <armcdowell@hotmail.com>
2014-10-21 16:10:27 +1300

    Added Dispose Property Task along with supporting functions

---
+++ changeset/sl_extension_v015.sql
@@ -0,0 +1,82 @@
+﻿-- 21 Oct 2015
+-- Add service to dispose state land property. 
+INSERT INTO application.request_type(code, request_category_code, display_value, 
+            status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, 
+            nr_properties_required, notation_template, rrr_type_code, type_action_code, 
+            description, display_group_name, service_panel_code)
+    SELECT 'disposeSLProperty','stateLandServices','Dispose Property','c',5,0.00,0.00,0.00,0,
+	null,null,'cancel','Updates a State Land Property to indicate the state has disposed of it','General', 'slProperty'
+	WHERE NOT EXISTS (SELECT code FROM application.request_type WHERE code = 'disposeSLProperty');
+	
+INSERT INTO system.approle (code, display_value, status, description)
+SELECT 'disposeSLProperty', 'Service - Dispose Property','c', 'State Land Service. Allows the Dispose Property service to be started.'
+WHERE NOT EXISTS (SELECT code FROM system.approle WHERE code = 'disposeSLProperty');
+
+INSERT INTO system.approle_appgroup (approle_code, appgroup_id) 
+    (SELECT 'disposeSLProperty', ag.id FROM system.appgroup ag WHERE ag."name" = 'Super group'
+	 AND NOT EXISTS (SELECT approle_code FROM system.approle_appgroup 
+	                 WHERE  approle_code = 'disposeSLProperty'
+					 AND    appgroup_id = ag.id));
+					 
+					 
+
+-- Update target-ba_unit-check-if-pending to include check for new request type					 
+ UPDATE system.br_definition
+ SET  body = 
+ 'WITH	otherCancel AS	(SELECT (SELECT (COUNT(*) = 0)FROM administrative.ba_unit_target ba_t2 
+				INNER JOIN transaction.transaction tn ON (ba_t2.transaction_id = tn.id)
+				WHERE ba_t2.ba_unit_id = ba_t.ba_unit_id
+				AND ba_t2.transaction_id != ba_t.transaction_id
+				AND tn.status_code != ''approved'') AS chkOther
+			FROM administrative.ba_unit_target ba_t
+			WHERE ba_t.ba_unit_id = #{id}), 
+	cancelAp AS	(SELECT ap.id FROM administrative.ba_unit_target ba_t 
+			INNER JOIN application.application_property pr ON (ba_t.ba_unit_id = pr.ba_unit_id)
+			INNER JOIN application.service sv ON (pr.application_id = sv.application_id)
+			INNER JOIN application.application ap ON (pr.application_id = ap.id)
+			WHERE ba_t.ba_unit_id = #{id}
+			AND sv.request_type_code IN (''cancelProperty'', ''disposeSLProperty'')
+			AND sv.status_code != ''cancelled''
+			AND ap.status_code NOT IN (''annulled'', ''approved'')),
+	otherAps AS	(SELECT (SELECT (count(*) = 0) FROM administrative.ba_unit ba
+			INNER JOIN administrative.rrr rr ON (ba.id = rr.ba_unit_id)
+			INNER JOIN transaction.transaction tn ON (rr.transaction_id = tn.id)
+			INNER JOIN application.service sv ON (tn.from_service_id = sv.id)
+			INNER JOIN application.application ap ON (sv.application_id = ap.id)
+			WHERE ba.id = #{id} 
+			AND ap.status_code = ''lodged''
+			AND ap.id NOT IN (SELECT id FROM cancelAp)) AS chkNoOtherAps),
+
+	pendingRRR AS	(SELECT (SELECT (count(*) = 0) FROM administrative.rrr rr
+				INNER JOIN administrative.ba_unit_target ba_t2 ON (rr.ba_unit_id = ba_t2.ba_unit_id)
+				INNER JOIN transaction.transaction t2 ON (ba_t2.transaction_id = t2.id)
+				INNER JOIN application.service s2 ON (t2.from_service_id = s2.id) 
+				WHERE ba_t2.ba_unit_id = ba_t.ba_unit_id
+				AND s2.application_id != s1.application_id
+				AND ba_t2.transaction_id != ba_t.transaction_id
+				AND rr.status_code = ''pending'') AS chkPend 
+			FROM administrative.ba_unit_target ba_t
+			INNER JOIN transaction.transaction t1 ON (ba_t.transaction_id = t1.id)
+			INNER JOIN application.service s1 ON (t1.from_service_id = s1.id) 
+			WHERE ba_t.ba_unit_id = #{id})
+SELECT ((SELECT chkPend  FROM pendingRRR) AND (SELECT chkOther FROM otherCancel)  AND (SELECT chkNoOtherAps FROM otherAps)) AS vl 
+FROM administrative.ba_unit_target tg
+WHERE tg.ba_unit_id  = #{id}'
+
+WHERE br_id = 'target-ba_unit-check-if-pending'; 
+
+
+-- Update ba_unit-has-a-valid-primary-right to include check for new request type	
+ UPDATE system.br_definition
+ SET  body = '
+SELECT (COUNT(*) = 1) AS vl FROM administrative.rrr rr1 
+	 INNER JOIN administrative.ba_unit ba ON (rr1.ba_unit_id = ba.id)
+	 INNER JOIN transaction.transaction tn ON (rr1.transaction_id = tn.id)
+	 INNER JOIN application.service sv ON ((tn.from_service_id = sv.id) 
+	      AND (sv.request_type_code NOT IN (''cancelProperty'', ''disposeSLProperty'')))
+ WHERE ba.id = #{id}
+ AND rr1.status_code != ''cancelled''
+ AND rr1.is_primary
+ AND rr1.type_code IN (''ownership'', ''apartment'', ''stateOwnership'', ''lease'')'
+ WHERE br_id = 'ba_unit-has-a-valid-primary-right'; 
+					 
